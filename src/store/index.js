5f2d44a49e22050104f6e4bd319b9d2726a3aee6
Tanuj <tanuj.lalwani@protonmail.com>
2019-12-26 15:49:42 -0800

    [STATE]: Setup table state management

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,22 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    t_rows: 2000,
+    t_cols: 10,
+    t_data: null
+  },
+  getters: {
+    getTableData: state => {
+      return state.t_data;
+    },
+    getCellData: state => {
+      return state.t_data;
+    }
+  },
   mutations: {},
-  actions: {},
-  modules: {}
+  actions: {}
 });
