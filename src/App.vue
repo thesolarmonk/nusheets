6c64a4a001f4e5ccf22f4cadd56173d0fec969d3
Tanuj <tanuj.lalwani@protonmail.com>
2019-12-26 17:25:18 -0800

    [STYLE]: Add CSS reset
    Added to clear default browser styles

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,65 @@
 <template>
-  <div id="app">
+  <div class="app">
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+/* CSS Reset */
+@import "./styles/_reset.scss";
+
+/* Font Imports */
+@import url("https://rsms.me/inter/inter.css");
+html {
+  font-family: "Inter", sans-serif;
+}
+
+/* Font Settings */
+
+* {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+}
+
+/* Layout Settings */
+
+* {
+  box-sizing: border-box;
+  -webkit-overflow-scrolling: touch;
+}
+
+/* CSS Global Style Variables */
+
+:root {
+  --background-color-default: #fff;
+  --primary-color: #fff;
+
+  --accent-color: rgb(41, 111, 252);
+
+  --font-color-default: #000;
+
+  --font-family: "Inter", sans-serif;
+  --font-style-default: normal;
+  --font-weight-normal: 400;
+  --font-weight-bold: 600;
+
+  --font-line-height-default: 1;
+  --font-letter-spacing-default: -0.003rem;
+  --font-word-spacing-default: -0.2rem;
+  --font-variant-ligatures: none;
+}
+
+/* App Layout */
+
+html,
+body,
+.app {
+  background-color: var(--background-color-default);
+  z-index: 0;
+}
+
+.app {
+  width: 100vw;
+  height: 100vh;
 }
 </style>
