82a77f21a4c662bb1a6f8d72f2f6b1d8d0732b75
Tanuj <tanuj.lalwani@protonmail.com>
2019-12-26 16:43:55 -0800

    [COMPONENT]: Initialize sheet data on load

--- src/components/Sheet.vue
+++ src/components/Sheet.vue
@@ -3,11 +3,16 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters, mapMutations } from "vuex";
 
 export default {
   computed: {
     ...mapGetters(["s_rows", "s_cols", "s_data", "s_exp"])
+  methods: {
+    ...mapMutations(["createTable"])
+  },
+  mounted() {
+    this.createTable();
   }
 };
 </script>
