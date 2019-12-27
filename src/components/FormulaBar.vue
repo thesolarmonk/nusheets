ff20db0d02feec0e578a7c36647ba71cd9921b65
Tanuj <tanuj.lalwani@protonmail.com>
2019-12-26 18:55:02 -0800

    [COMPONENT]: Create FormulaBar component

---
+++ src/components/FormulaBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <input class="formula-bar" type="text" />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.formula-bar {
+  width: 100%;
+  padding: 5px 5px 5px 15px;
+  height: 40px;
+
+  font-size: var(--font-size-default);
+
+  outline: none;
+  border: 1px solid var(--primary-color-dark);
+  background-color: var(--primary-color);
+}
+</style>
