f74be80b85a18e08bf46ec53bcedb74b6d31076c
Tanuj <tanuj.lalwani@protonmail.com>
2019-12-26 16:09:15 -0800

    [COMPONENT]: Add state getters to Sheet component

---
+++ src/components/Sheet.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sheet">
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["s_rows", "s_cols", "s_data", "s_exp"])
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
