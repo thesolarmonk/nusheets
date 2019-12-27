f505f3f66c9136234b396abab2a643590d3b1eca
Tanuj <tanuj.lalwani@protonmail.com>
2019-12-26 17:16:07 -0800

    [COMPONENT]: Add HTML table
    The table element is binded to the sheet's data

--- src/components/Sheet.vue
+++ src/components/Sheet.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="sheet">
+  <div class="sheet-container">
+    <table class="sheet">
+      <tbody>
+        <tr class="sheet__row">
+          <th class="cell cell--corner"></th>
+          <th
+            class="cell cell--header cell--column-header"
+            v-for="(col, n_col) in col_headers"
+            :key="n_col"
+          >{{ col }}</th>
+        </tr>
+        <tr class="sheet__row" v-for="(row, n_row) in s_data" :key="n_row">
+          <th class="cell cell--header cell--row-header">{{ n_row + 1 }}</th>
+          <td
+            class="cell cell--data"
+            v-for="(cell, n_col) in row"
+            :key="n_row + '_' + n_col"
+            :data-row="n_row"
+            :data-column="n_col"
+          >{{ s_data[n_row][n_col] }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
