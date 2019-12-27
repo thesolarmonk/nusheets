96281e37353d3c7fa8fb019277a5ac0d9dcea24a
Tanuj <tanuj.lalwani@protonmail.com>
2019-12-26 16:44:52 -0800

    [COMPONENT]: Compute column headers as alphabets

--- src/components/Sheet.vue
+++ src/components/Sheet.vue
@@ -7,7 +7,13 @@ import { mapGetters, mapMutations } from "vuex";
 
 export default {
   computed: {
-    ...mapGetters(["s_rows", "s_cols", "s_data", "s_exp"])
+    ...mapGetters(["s_rows", "s_cols", "s_data", "s_exp"]),
+    col_headers: function() {
+      return [...Array(this.s_cols).keys()].map(col =>
+        String.fromCharCode(97 + col).toUpperCase()
+      );
+    }
+  },
   methods: {
     ...mapMutations(["createTable"])
   },
